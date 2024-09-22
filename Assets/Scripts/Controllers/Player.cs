46b5d326d15fa1ad7490fe8d5200b64ded2c89ea
Michelle <148483702+michubishi@users.noreply.github.com>
2024-09-21 22:07:54 -0400

    Added player movement (1a done)
    Added method that takes in player input.

--- Assets/Scripts/Controllers/Player.cs
+++ Assets/Scripts/Controllers/Player.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using JetBrains.Annotations;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -8,10 +9,35 @@ public class Player : MonoBehaviour
     public Transform enemyTransform;
     public GameObject bombPrefab;
     public Transform bombsTransform;
+    public Vector3 velocity;
 
     void Update()
     {
+        PlayerMovement();
 
+    }
+
+    public void PlayerMovement()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.position = transform.position + (velocity = new Vector3(-0.01f, 0f));
+        }
+
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.position = transform.position + (velocity = new Vector3(0.01f, 0f));
+        }
+
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.position = transform.position + (velocity = new Vector3(0f, 0.01f));
+        }
+
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.position = transform.position + (velocity = new Vector3(0f, -0.01f));
+;        }
     }
 
 }
